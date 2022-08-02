09ed2588a437805d4b98df15d0c566d33fd595ec
Shimon Asulin <asulin.shimon699@gmail.com>
2022-08-02 22:51:36 +0300

    playing with JS DOM

---
+++ FastWhatsApp/main.js
@@ -0,0 +1,31 @@
+const header = document.getElementById('header');
+let dropdown = document.getElementById('dropdown');
+let input = document.getElementById('input');
+const button = document.getElementById('send');
+const edit = document.getElementById('edit-name');
+let user =  localStorage.getItem("user");
+if (!user) {    
+     user = prompt("Please enter your name😎")
+}
+
+localStorage.setItem("user", user )
+header.textContent += user;
+// collect
+let quickWhatsApp = null
+button.addEventListener("click", () => {
+    quickWhatsApp = `https://wa.me/${dropdown.value}${input.value}`
+    window.open(quickWhatsApp, '_blank');
+    // alert(quickWhatsApp);
+})
+// keyboard key
+input.addEventListener("keyup", (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    document.getElementById('send').click();
+  }
+});
+// change name
+edit.addEventListener("click", () => {
+    localStorage.clear()
+    location.reload();
+})
